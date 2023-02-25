337a3a5c99f3166618226f2821c3d7a8f05a3539
KDYa08 <owen0821@naver.com>
2023-02-26 02:44:24 +0900

    second commit

--- script.js
+++ script.js
@@ -21,3 +21,73 @@ function arenaSweep(){
 
     }
 }
+
+function collide(arena, player){
+
+    const m = player.matrix;
+    const o = player.pos;
+    for(let y = 0; y < m.length; ++y){
+        for(let x = 0; x < m[y].length; ++x){
+            if(m[y][x] !== 0 && (arena[y + o.y][x + o.x]) !==0){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+function createMatrix(w, h){
+
+    const matrix = [];
+    whhile(h--){
+        matrix.push(new Array(w).fill(0));
+    }
+    return matrix;
+}
+
+function createPiece(type){
+
+    if(type === "I"){
+        return[
+            [0, 1, 0, 0],
+            [0, 1, 0, 0],
+            [0, 1, 0, 0],
+            [0, 1, 0, 0]
+        ];
+    }else if(type === "L"){
+        return[
+            [0, 2, 0],
+            [0, 2, 0],
+            [0, 2, 2]
+        ];
+    }else if(type === "J"){
+        return[
+            [0, 3, 0],
+            [0, 3, 0],
+            [3, 3, 0]
+        ];
+    }else if(type === "O"){
+        return[
+            [4, 4],
+            [4, 4]
+        ];
+    }else if(type === "2"){
+        return[
+            [5, 5, 0],
+            [0, 5, 5],
+            [0, 0, 0]
+        ];
+    }else if(type === "S"){
+        return[
+            [0, 6, 6],
+            [6, 6, 0],
+            [0, 0, 0]
+        ]
+    }else if(type === "T"){
+        return[
+            [0, 7, 0],
+            [7, 7, 7],
+            [0, 0, 0]
+        ];
+    }
+}
